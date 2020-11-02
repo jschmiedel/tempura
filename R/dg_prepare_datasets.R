7abc08fc62e25f935db0c9467dcf01551f953d95
jschmiedel <joern.schmiedel@gmail.com>
2020-11-02 14:48:39 +0100

    support for aa_subs only column in input files

--- R/dg_prepare_datasets.R
+++ R/dg_prepare_datasets.R
@@ -2,9 +2,9 @@
 #'
 #'
 #' @param dataset_folder absolute path to the dataset folder, is created if non-existent
-#' @param abundancepca_files absolute path to the DiMSum-processed abundancePCA .RData files containing aa_seq, fitness and sigma cols; multiple files will be treated as independent measurements
-#' @param bindingpca_files absolute path to the DiMSum-processed bindingPCA .RData files containing aa_seq, fitness and sigma cols; multiple files will be used as independent measurements
-#' @param wt_seq a character string of the wild-type/reference aa sequence
+#' @param abundancepca_files absolute path to the DiMSum-processed abundancePCA .RData files containing aa_seq (or aa_subs), fitness and sigma cols; multiple files will be treated as independent measurements
+#' @param bindingpca_files absolute path to the DiMSum-processed bindingPCA .RData files containing aa_seq (or aa_subs), fitness and sigma cols; multiple files will be used as independent measurements
+#' @param wt_seq a character string of the wild-type/reference aa sequence used to extract aa_subs from aa_seq; if wt_seq is empty, assumes aa_subs column is present
 #' @param train_test_split c(A, B), Ax test split of variants, where each split has size B; default c(10, 0.1), i.e. 10x cross validation
 #' @param fitness_scale either "lin" or "log"; for "lin", assumes fitness = [0,Inf], with wild-type fitness 1; for "log", assumes fitness = [-Inf, Inf], with wild-type fitness = 0, default = "lin"
 #'
@@ -16,7 +16,7 @@ dg_prepare_datasets = function(
 	dataset_folder,
 	abundancepca_files,
 	bindingpca_files,
-	wt_seq,
+	wt_seq = "",
 	train_test_split = c(10, 0.1),
 	fitness_scale = "lin"
 ) {
@@ -31,6 +31,13 @@ dg_prepare_datasets = function(
 	all_files = list()
 	no_abd_datasets <- length(abundancepca_files)
 	no_bind_datasets <- length(bindingpca_files)
+
+  if (wt_seq != "") {
+    common_col = "aa_seq"
+  } else {
+    common_col = "aa_subs"
+  }
+
 	# load abundancePCA files
 	for (i in seq_along(abundancepca_files)) {
 		load(abundancepca_files[i])
@@ -38,7 +45,7 @@ dg_prepare_datasets = function(
 		names(all_variants)[grep("^sigma$", names(all_variants))] <- paste0("f", i, "_sigma")
 		all_files[[i]] <- all_variants[, #only allow missense variants
 			.SD,
-			.SDcols = c("aa_seq",
+			.SDcols = c(common_col,
 				paste0("f", i, "_fitness"),
 				paste0("f", i, "_sigma"))]
 	}
@@ -49,14 +56,14 @@ dg_prepare_datasets = function(
 		names(all_variants)[grep("^sigma$", names(all_variants))] <- paste0("b", i, "_sigma")
 		all_files[[i + no_abd_datasets]] <- all_variants[,  #only allow missense variants
 			.SD,
-			.SDcols = c("aa_seq",
+			.SDcols = c(common_col,
 				paste0("b", i, "_fitness"),
 				paste0("b", i, "_sigma"))]
 	}
 	# merge files
 	variant_data <- all_files[[1]]
 	for (i in 2:length(all_files)) {
-		variant_data <- merge(variant_data, all_files[[i]], by = "aa_seq", all = T)
+		variant_data <- merge(variant_data, all_files[[i]], by = common_col, all = T)
 	}
 
 
@@ -71,12 +78,14 @@ dg_prepare_datasets = function(
 	variant_data[, tts := NULL]
 
 
-	## extract aa mutations from aa_seq
-	wt_seq_split <- strsplit(wt_seq, "")[[1]]
-	variant_data[, aa_subs := paste0(stringr::str_pad(which(strsplit(aa_seq, "")[[1]] != wt_seq_split), ceiling(log10(length(wt_seq_split))), "left", "0"),
-			strsplit(aa_seq, "")[[1]][which(strsplit(aa_seq, "")[[1]] != wt_seq_split)],
-			collapse = "_"),
-		aa_seq]
+  if (wt_seq != "") {
+    ## extract aa mutations from aa_seq
+    wt_seq_split <- strsplit(wt_seq, "")[[1]]
+    variant_data[, aa_subs := paste0(stringr::str_pad(which(strsplit(aa_seq, "")[[1]] != wt_seq_split), ceiling(log10(length(wt_seq_split))), "left", "0"),
+        strsplit(aa_seq, "")[[1]][which(strsplit(aa_seq, "")[[1]] != wt_seq_split)],
+        collapse = "_"),
+      aa_seq]
+  }
 	data.table::setkey(variant_data, aa_subs)
 
 
